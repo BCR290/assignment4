75de407c608c7aea87ba1a6e04e0f20152784b72
TheCmar7 <smithcr@flip3.engr.oregonstate.edu>
2015-07-14 21:04:20 -0700

    I found the correct url and styled the javascript a bit better I am on a breakthough, but I need to go to the bar, or dinner or something so I will com back to this. I am working on it, I will show you tomorrow.

--- contol.js
+++ contol.js
@@ -1,8 +1,45 @@
 // Javascrpt
-var page_num = 1;
-var selectPageNum = document.getElementByName("pages");
+"use strict";
 
-var loadTasks = function() {
-	//get user settings from local storage
+//Ok 
+(function() {
+	var _url = "https://api.github.com/gists/";
 	
-}
+
+	window.onload = function() {
+		makeAjaxCall(_url);
+
+		var selectPageNum = document.getElementByName("pages");
+		selectPageNum.onclick = changePageSize();
+
+		// other variables for other numbers
+	}
+
+	var makeAjaxCall = function(url) {
+		// build the table
+		var request = new XMLHttpRequest();
+		request.open("GET", url, true);
+		request.send(null);
+
+		request.onreadystatechange = function() {
+			if (request.readyState === 4) {
+				if (request.status === 200) { 
+					//responseText
+					var serverResponse = JSON.parse(request.responseText);
+					var gists = serverResponse.array;
+					var gistsArray = [];
+					for(var i=0;i<memes.length;i++){
+						var m = new meme(memes[i].name, memes[i].url);
+						memeArray.push(m);
+						m.convertToHtml();
+					}     
+				} else {
+					alert("Error!");
+				}
+			}
+		}
+	}
+
+	function gists()
+
+}();

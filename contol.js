0716960f1ae516615d85f0815b50e01edf2cfb06
TheCmar7 <smithcr@flip2.engr.oregonstate.edu>
2015-07-16 13:05:42 -0700

    I have added some stuff to make the styling a lot easier

--- contol.js
+++ contol.js
@@ -33,7 +33,7 @@
 					var gists = serverResponse;
 					var gistsArray = [];
 					for(var i = 0; i < gists.length; i++){
-						var g = new gist(gists[i].url, gists[i].id, gists[i].description);
+						var g = new gist(gists[i].url, gists[i].id, gists[i].description, gists[i].html_url);
 						gistsArray.push(g);
 						g.convertToHtml();
 					}    
@@ -45,27 +45,32 @@
 	}
 
 	//object of a gist 
-	function gist(url, id, description) {
-		this.cellNum = 3;	// number of cells we will create for each gist (subject to change)
+	function gist(url, id, description, html) {
+		this.cellNum = 4;	// number of cells we will create for each gist (subject to change)
 		
 		this.url = url;		// the URL of the gist
 		this.id = id;		// The id number? I don't know if this is needed
 		this.description = description;	//The description of the gitst
+		this.html_url = html;
 
 		// a function for making a gist into an HTML element (part of the table)
 		this.convertToHtml = function() {
 			// This is called everytime we make an gist...
-			var row = document.createElement("tr");
+			var row = document.createElement("div");
 			row.className = "gistRow";
-			for (var i = 0; i < this.cellNum; i++) {
-				var cell = document.createElement("td");
+			for (var i = 0; i <= this.cellNum; i++) {
+				var cell = document.createElement("div");
 				cell.className = "cell";
 				if (i == 0) {
-					cell.innerHTML = this.url;
+					cell.innerHTML = "Favorite this gist: <input type=\"checkbox\" id=\"" + this.description +"\">";
 				} else if (i == 1) {
-					cell.innerHTML = this.id;
+					cell.innerHTML = "<span class=\"info\">URL:</span> " + this.url;
 				} else if (i == 2) {
-					cell.innerHTML = this.description;
+					cell.innerHTML = "<span class=\"info\">ID:</span> " + this.id;
+				} else if (i == 3) {
+					cell.innerHTML = "<span class=\"info\">Description:</span> " + this.description;
+				} else if (i == 4) {
+					cell.innerHTML = "<span class=\"info\">HTML:</span> <a href=\"" + this.html_url + "\">" + this.html_url + "</a>";
 				}
 				row.appendChild(cell);
 				document.getElementById("infoTable").appendChild(row);
@@ -76,19 +81,17 @@
 
 	//This function changes the page number that we are looking at
 	var changePageSize = function() {
-		console.log("popp");
 		var page = document.getElementById("page_amount").value;
-		console.log(page)
 		_url = "https://api.github.com/gists" + page;
-		console.log(_url);
   		deleteTable();
 		makeAjaxCall(_url);
 	}
 
 	// This functino creates a table that will get filled with GISTS
 	var createTable = function() {
-		var table = document.createElement("table"); //making a table
+		var table = document.createElement("feildset"); //making a table
 		table.id = "infoTable";
+
 		var infoArea = document.getElementById("infoArea"); // put table on HTML
 		infoArea.appendChild(table); 
 	}

4a20c5cf7a15c3e2472b9cf8bd9dd14d6159346c
TheCmar7 <smithcr@flip2.engr.oregonstate.edu>
2015-07-16 12:13:50 -0700

    This version works! It deleted the table and makes a new one

--- contol.js
+++ contol.js
@@ -19,6 +19,8 @@
 
 	var makeAjaxCall = function(url) {
 		// build the table
+		createTable();
+
 		var request = new XMLHttpRequest();
 		request.open("GET", url, true);
 		request.send(null);
@@ -34,7 +36,7 @@
 						var g = new gist(gists[i].url, gists[i].id, gists[i].description);
 						gistsArray.push(g);
 						g.convertToHtml();
-					}     
+					}    
 				} else {
 					alert("Error!");
 				}
@@ -45,12 +47,14 @@
 	//object of a gist 
 	function gist(url, id, description) {
 		this.cellNum = 3;	// number of cells we will create for each gist (subject to change)
+		
 		this.url = url;		// the URL of the gist
 		this.id = id;		// The id number? I don't know if this is needed
 		this.description = description;	//The description of the gitst
 
 		// a function for making a gist into an HTML element (part of the table)
 		this.convertToHtml = function() {
+			// This is called everytime we make an gist...
 			var row = document.createElement("tr");
 			row.className = "gistRow";
 			for (var i = 0; i < this.cellNum; i++) {
@@ -64,25 +68,34 @@
 					cell.innerHTML = this.description;
 				}
 				row.appendChild(cell);
+				document.getElementById("infoTable").appendChild(row);
 			}
-			var table = document.getElementById("gistlist");
-			table.appendChild(row);
 		}
 		// end of function
 	}
 
+	//This function changes the page number that we are looking at
 	var changePageSize = function() {
 		console.log("popp");
 		var page = document.getElementById("page_amount").value;
 		console.log(page)
 		_url = "https://api.github.com/gists" + page;
 		console.log(_url);
-
-		// var rows = document.getElementsByName("gistRow");
-  //       document.removeChild(rows);
-		
+  		deleteTable();
 		makeAjaxCall(_url);
-		
+	}
+
+	// This functino creates a table that will get filled with GISTS
+	var createTable = function() {
+		var table = document.createElement("table"); //making a table
+		table.id = "infoTable";
+		var infoArea = document.getElementById("infoArea"); // put table on HTML
+		infoArea.appendChild(table); 
+	}
+
+	// This function delets the table that we created 
+	var deleteTable = function() {
+		document.getElementById("infoArea").removeChild(document.getElementById("infoTable")); // Deleted the table from the HTML
 	}
 
 })();

a0fc39396dd10fd7bffd5465be7c50022f5d7ce1
Bradley Imai <imaib@onid.oregonstate.edu>
2015-07-17 17:39:23 -0700

    add to favorites and display on the screen however, we need to figure out a few more things

--- contol.js
+++ contol.js
@@ -24,7 +24,8 @@
  
                 var searcher = document.getElementById("searchButton");
                 searcher.onclick = searchArray;
- 
+                
+                loadfavorites;
  
         }
  
@@ -33,15 +34,15 @@
                         deleteTable();
  
                         var table = document.createElement("feildset"); //making a table
-            table.id = "infoTable";
-            var infoArea = document.getElementById("infoArea"); // put table on HTML
-            infoArea.appendChild(table);
+                        table.id = "infoTable";
+                        var infoArea = document.getElementById("infoArea"); // put table on HTML
+                        infoArea.appendChild(table);
  
                         var numOfResults = 0;
                         for (var x = 0; x < gistsArray.length; x ++){
  
                                 if (gistsArray[x].description == document.getElementById("searchText").value){
-                                        gistsArray[x].convertToHtml();
+                                        gistsArray[x].convertToHtml("infoTable");
                                         numOfResults ++ ;
  
                                 }
@@ -51,7 +52,20 @@
                                 document.getElementById("infoArea").innerHTML = "No Gists found!!!";
                         }
                 }
- 
+
+        var loadfavorites = function(){
+            favoriteArray = JSON.parse(localStorage.getItem("favorites"));
+            for(var q = 0; q < favoriteArray.length; q++){
+                console.log(favoriteArray[q]);
+                var holder = document.createElement("div");
+                holder.className = "favoriteGist";
+                
+                holder.innerHTML = favoriteArray[q].url + "<a href=\"" +favoriteArray[q].html_url + "\">" +  favoriteArray[q].description + "</a>";
+
+                document.getElementById("favoritesbar").appendChild(holder);
+                
+            }
+        }
  
         //This is after the ajax call...
         var doEveryThing = function() {
@@ -78,7 +92,7 @@
                                 if (request.status === 200) {
                                         var gists = JSON.parse(request.responseText);
                                         for (var j = 0; j < gists.length; j++) {
-                                                var g = new gist(gists[j].url, gists[j].id, gists[j].description, gists[j].html_url);
+                                                var g = new gist(gists[j].url, gists[j].description, gists[j].html_url);
                                                 gistsArray.push(g);    
  
                                                 if (j == 1) {
@@ -95,18 +109,18 @@
         }
  
         //object of a gist
-        function gist(url, id, description, html) {
-                this.cellNum = 4;       // number of cells we will create for each gist (subject to change)
+        function gist(url, description, html) {
+                this.cellNum = 2;       // number of cells we will create for each gist (subject to change)
                
                 this.url = url;         // the URL of the gist
-                this.id = id;           // The id number? numOfAjaxCalls don't know if this is needed
+                //this.id = id;           // The id number? numOfAjaxCalls don't know if this is needed
                 this.description = description; //The description of the gitst
                 this.html_url = html;
  
                 // a function for making a gist into an HTML element (part of the table)
-                this.convertToHtml = function() {
-                        var table = document.getElementById("gistlist");      
-                        console.log("html");
+                this.convertToHtml = function(place) {
+                        //var table = document.getElementById("gistlist");      
+                        //console.log("html");
                         // This is called everytime we make an gist...
                         var row = document.createElement("div");
                         row.className = "gistRow";
@@ -114,31 +128,35 @@
                                 var cell = document.createElement("div");
                                 cell.className = "cell";
                                 if (t == 0) {
-                                        var gistID = this.id;
+                                        
+                                        var gistURL = this.url;
+                                        var gistDesc = this.description;
+                                        var gistHTML = this.html_url;
+                                        var savedGist = new gist(gistURL, gistDesc, gistHTML); 
                                         //console.log("1 " + gistID)
                                         var taskRemove = document.createElement("Button");
                                         taskRemove.className = "btn btn-danger pull-right";
                                         taskRemove.innerHTML = "Favorite This Gist";
                                         //taskRemove.setAttribute("gistID", this.id);
                                         taskRemove.onclick = function() {
-                                            console.log("2 " + gistID);
-                                            favoriteArray.push(gistID);
+                                            //console.log("2 " + gistID);
+                                            favoriteArray.push(savedGist);
                                             localStorage.setItem("favorites", JSON.stringify(favoriteArray));
                                             console.log("pop");
+                                            loadfavorites();
                                         }
                                         cell.appendChild(taskRemove);
                                 } else if (t == 1) {
                                         cell.innerHTML = "<span class=\"info\">URL:</span> " + this.url;
                                 } else if (t == 2) {
-                                        cell.innerHTML = "<span class=\"info\">ID:</span> " + this.id;
-                                } else if (t == 3) {
-                                        cell.innerHTML = "<span class=\"info\">Description:</span> " + this.description;
-                                } else if (t == 4) {
-                                        cell.innerHTML = "<span class=\"info\">HTML:</span> <a href=\"" + this.html_url + "\">" + this.html_url + "</a>";
+                                        if(this.description == "") {
+                                            this.description = "No description given";
+                                        }
+                                        cell.innerHTML = "<span class=\"info\">HTML:</span> <a href=\"" + this.html_url + "\">" + this.description + "</a>";
                                 }
                                 row.appendChild(cell);
                         }
-                        document.getElementById("infoTable").appendChild(row);
+                        document.getElementById(place).appendChild(row);
                 }
                 // end of function
         }
@@ -163,7 +181,7 @@
                         //console.log("bleh");
                       //  console.log(gistsArray[w]);
                     if (gistsArray[w] !== 'undefined')        
-                       gistsArray[w].convertToHtml();
+                       gistsArray[w].convertToHtml("infoTable");
                 }
         }
  

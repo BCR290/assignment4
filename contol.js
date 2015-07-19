562640a4bc03015f1957231ff452fa5939dd3d24
Yipeng Song <songyip@flip3.engr.oregonstate.edu>
2015-07-18 17:41:35 -0700

    I deleted all console.log which are used for debugging.

--- contol.js
+++ contol.js
@@ -15,6 +15,7 @@
     // This will be called when the variable is created.
     window.onload = function() {
         var pages = ["?page=1", "?page=2", "?page=3", "?page=4", "?page=5"];
+
         for (numOfAjaxCalls = 0; numOfAjaxCalls < pages.length; numOfAjaxCalls ++) {
             var theurl = _url + pages[numOfAjaxCalls ];
             makeAjaxCall(theurl);             //Makes the ajax calls and fills THIS NEEDS TO BE CHANGED
@@ -31,56 +32,50 @@
         } else {
             loadfavorites;
         }
-
     }
-
 
     var searchArray = function (){
         deleteTable();
+
         var table = document.createElement("feildset"); //making a table
         table.id = "infoTable";
+
         var infoArea = document.getElementById("infoArea"); // put table on HTML
         infoArea.appendChild(table);
+
         for (var x = 0; x < gistsArray.length; x ++){
             if (gistsArray[x].description == document.getElementById("searchText").value) {
                 var counter = 0;
 
                 for (var y = 0; y < favoriteArray.length; y++){
-                    if (gistsArray[x].html_url == favoriteArray[y].html_url){
-                        counter ++;
-                                           
-                    }
+                    if (gistsArray[x].html_url == favoriteArray[y].html_url)
+                        counter ++;                                                               
                 }
                 
-                if (counter == 0){
-                    gistsArray[x].convertToHtml("infoTable"); 
-                }
-
+                if (counter == 0)
+                    gistsArray[x].convertToHtml("infoTable");                 
             }
         }
-
-
     }
 
     var loadfavorites = function(){
-        if (document.getElementById("favoriteGist") != null) {
-            deleteFavs();
-        }
+        if (document.getElementById("favoriteGist") != null) 
+            deleteFavs();       
         
         favoriteArray = JSON.parse(localStorage.getItem("favorites"));
-        if (favoriteArray == null) {
+
+        if (favoriteArray == null) 
             localStorage.setItem("favorites", JSON.stringify(favoriteArray));
-        }
-        favoriteArray = JSON.parse(localStorage.getItem("favorites"));
-        //console.log(favoriteArray.length);
         
+        favoriteArray = JSON.parse(localStorage.getItem("favorites"));
+                
         var holder = document.createElement("div");
         holder.id = "favoriteGist";
-        for(var q = 0; q < favoriteArray.length; q++){
-            console.log("creating favorites: " + favoriteArray[q]);
 
+        for(var q = 0; q < favoriteArray.length; q++){            
             var oneFavorite = document.createElement("div");
             oneFavorite.className = "favoriteOne";
+
             if (favoriteArray[q].description == "No description given") {
                 oneFavorite.innerHTML = "<a href=\"" + favoriteArray[q].html_url + "\">" +  favoriteArray[q].html_url + "</a>";
             } else { 
@@ -92,13 +87,13 @@
             deleteFavorite.innerHTML = "&times;";
             deleteFavorite.setAttribute("index", q);
             deleteFavorite.onclick = function() {
-                console.log(this.getAttribute("index"));
-                console.log(favoriteArray);
-                favoriteArray.splice(parseInt(this.getAttribute("index"), 10), 1);
-                console.log(favoriteArray);
-                localStorage.setItem("favorites", JSON.stringify(favoriteArray));
-                deleteFavs();
-                loadfavorites();
+                
+                
+            favoriteArray.splice(parseInt(this.getAttribute("index"), 10), 1);
+                
+            localStorage.setItem("favorites", JSON.stringify(favoriteArray));
+            deleteFavs();
+            loadfavorites();
             }
                         
             oneFavorite.appendChild(deleteFavorite);
@@ -125,13 +120,10 @@
         gistsArray.push(g);
     }
 
-    var makeAjaxCall = function(url) {
-        //console.log("ajax");
-        var request = new XMLHttpRequest();
-        console.log(url);
+    var makeAjaxCall = function(url) {        
+        var request = new XMLHttpRequest();       
         request.open("GET", url, true);
-        request.send(null);
-       
+        request.send(null);      
         request.onreadystatechange = function() {
             if (request.readyState === 4) {
                 if (request.status === 200) {
@@ -150,17 +142,15 @@
 
     //object of a gist
     function gist(url, description, html) {
-        this.cellNum = 2;       // number of cells we will create for each gist (subject to change)
-       
-        this.url = url;         // the URL of the gist
-        //this.id = id;           // The id number? numOfAjaxCalls don't know if this is needed
+        this.cellNum = 2;       // number of cells we will create for each gist (subject to change)       
+        this.url = url;         // the URL of the gist        
         this.description = description; //The description of the gitst
         this.html_url = html;
 
         // a function for making a gist into an HTML element (part of the table)
         this.convertToHtml = function(place) {
             //var table = document.getElementById("gistlist");      
-            //console.log("html");
+            
             // This is called everytime we make an gist...
             var row = document.createElement("div");
             row.className = "gistRow";
@@ -173,16 +163,16 @@
                     var gistDesc = this.description;
                     var gistHTML = this.html_url;
                     var savedGist = new gist(gistURL, gistDesc, gistHTML); 
-                    //console.log("1 " + gistID)
+                    
                     var taskRemove = document.createElement("Button");
                     taskRemove.className = "btn btn-danger pull-right";
                     taskRemove.innerHTML = "Favorite This Gist";
                     //taskRemove.setAttribute("gistID", this.id);
                     taskRemove.onclick = function() {
-                        console.log("2 " + gistURL);
+                       
                         favoriteArray.push(savedGist);
                         localStorage.setItem("favorites", JSON.stringify(favoriteArray));
-                        //console.log("pop");
+                        
                         loadfavorites();
                     }
                     cell.appendChild(taskRemove);
@@ -203,23 +193,19 @@
 
 
     //This function changes the page number that we are looking at
-    var changePageSize = function() {
-                //document.getElementById("searchText").value = "";
+    var changePageSize = function() {                
         var page = document.getElementById("page_amount").value;
         deleteTable();
         createTable(page, gistsArray);
     }
 
     // This functino creates a table that will get filled with GISTS
-    var createTable = function(page) {
-        //console.log(page);
+    var createTable = function(page) {       
         var table = document.createElement("feildset"); //making a table
         table.id = "infoTable";
         var infoArea = document.getElementById("infoArea"); // put table on HTML
         infoArea.appendChild(table);
-        for (var w = (page * 30); w < ((page * 30) + 30); w++) {
-            //console.log("bleh");
-            //  console.log(gistsArray[w]);
+        for (var w = (page * 30); w < ((page * 30) + 30); w++) {                        
             if (typeof gistsArray[w] != 'undefined')        
                gistsArray[w].convertToHtml("infoTable");
         }
